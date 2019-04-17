17e8095d89236305650354f940ed22aeb0f2a23f
jsfnqcw <32922289+jsfnqcw@users.noreply.github.com>
2019-04-17 19:18:34 +0800

    1

---
+++ src/main/resources/static/js/sideMenu.js
@@ -0,0 +1,15 @@
+function studentAllClass(){
+  window.location.href = 'homepageST';
+}
+
+function studentChooseClass(){
+  window.location.href = 'electiveCurriculum';
+}
+
+function studentMessage(){
+  window.location.href = 'homepageST';
+}
+
+function studentSeettings(){
+  window.location.href = '/profileST#profile=1';
+}
